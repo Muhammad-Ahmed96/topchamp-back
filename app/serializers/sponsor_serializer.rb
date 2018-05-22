6df21cc8ef3de7c1c65f5e1d3d9e4977de572205
daniel flores sosa <danieldflrss@gmail.com>
2018-05-22 12:45:31 -0600

    id sponsor

--- app/serializers/sponsor_serializer.rb
+++ app/serializers/sponsor_serializer.rb
@@ -1,5 +1,5 @@
 class SponsorSerializer < ActiveModel::Serializer
-  attributes :company_name, :logo, :brand, :product, :franchise_brand, :business_category, :geography, :description, :contact_name,
+  attributes :id, :company_name, :logo, :brand, :product, :franchise_brand, :business_category, :geography, :description, :contact_name,
              :country_code, :phone, :email, :address_line_1, :address_line_2, :postal_code, :state, :city, :work_country_code,
              :work_phone, :status
 end
