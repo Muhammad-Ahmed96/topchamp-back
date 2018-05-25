26b3e2f0038f336774059048dfc7b77555f25d66
daniel flores sosa <danieldflrss@gmail.com>
2018-05-25 13:19:12 -0600

    delte event migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_164523) do
+ActiveRecord::Schema.define(version: 2018_05_23_225429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,11 +73,6 @@ ActiveRecord::Schema.define(version: 2018_05_25_164523) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_event_types_on_deleted_at"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "languages", force: :cascade do |t|
