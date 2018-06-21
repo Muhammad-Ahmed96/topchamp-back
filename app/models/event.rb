25bffdb22c804508d0289aff0eb9cf28faec4411
daniel flores sosa <danieldflrss@gmail.com>
2018-06-21 13:13:26 -0600

    Current enroll serializer

--- app/models/event.rb
+++ app/models/event.rb
@@ -514,6 +514,13 @@ class Event < ApplicationRecord
         key :'$ref', :EventAgenda
       end
     end
+
+    property :enrolls do
+      key :type, :array
+      items do
+        key :'$ref', :EventEnroll
+      end
+    end
   end
   swagger_schema :EventInput do
     key :required, [:event_type_id, :title, :description]
--- app/serializers/event_enroll_serializer.rb
+++ app/serializers/event_enroll_serializer.rb
@@ -2,7 +2,7 @@ class EventEnrollSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :event_id, :category_id, :event_bracket_age_id, :event_bracket_skill_id, :status
 
   belongs_to :user, serializer:UserSerializer
-  belongs_to :event, serializer:EventSerializer
+  belongs_to :event, serializer:SingleEventSerializer
   belongs_to :category, serializer:CategorySerializer
   belongs_to :bracket_skill, serializer:EventBracketSkillSerializer
   belongs_to :bracket_age, serializer:EventBracketAgeSerializer
