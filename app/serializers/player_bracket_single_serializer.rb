79d98708c98bddee24d2f4280a339140c61af6b3
daniel flores sosa <danieldflrss@gmail.com>
2018-07-11 12:26:35 -0600

    delegate destroy brackets

--- app/serializers/player_bracket_single_serializer.rb
+++ app/serializers/player_bracket_single_serializer.rb
@@ -4,27 +4,39 @@ class PlayerBracketSingleSerializer < ActiveModel::Serializer
   has_one :parent_bracket, serializer: EventBracketSingleSerializer
 
   def id
-    object.bracket.id
+    unless object.bracket.nil?
+      object.bracket.id
+    end
   end
 
   def age
-    object.bracket.age
+    unless object.bracket.nil?
+      object.bracket.age
+    end
   end
 
   def lowest_skill
-    object.bracket.lowest_skill
+    unless object.bracket.nil?
+      object.bracket.lowest_skill
+    end
   end
 
   def highest_skill
-    object.bracket.highest_skill
+    unless object.bracket.nil?
+      object.bracket.highest_skill
+    end
   end
 
   def quantity
-    object.bracket.quantity
+    unless object.bracket.nil?
+      object.bracket.quantity
+    end
   end
 
   def parent_bracket
-    EventBracket.where(:id => object.bracket.event_bracket_id).first
+    unless object.bracket.nil?
+      EventBracket.where(:id => object.bracket.event_bracket_id).first
+    end
   end
 
 end
