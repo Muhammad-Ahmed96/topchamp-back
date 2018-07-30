faba18332782c9f2af9b8bc07d8fcca1a8732233
daniel flores sosa <danieldflrss@gmail.com>
2018-07-30 13:16:12 -0600

    add bank_routing_number to payment informations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_182137) do
+ActiveRecord::Schema.define(version: 2018_07_30_191105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -236,6 +236,7 @@ ActiveRecord::Schema.define(version: 2018_07_28_182137) do
     t.text "refund_policy"
     t.float "service_fee"
     t.float "app_fee"
+    t.string "bank_routing_number"
   end
 
   create_table "event_payment_methods", force: :cascade do |t|
