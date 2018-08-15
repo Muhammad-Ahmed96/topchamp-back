91eae745de5b5a589703790eab906f1570177963
daniel flores sosa <danieldflrss@gmail.com>
2018-08-15 12:41:34 -0600

    Matches list doc

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -223,7 +223,58 @@ class TournamentsController < ApplicationController
     json_response_serializer(tournament, TournamentSerializer)
   end
 
-
+  swagger_path '/events/:event_id/tournaments/rounds' do
+    operation :get do
+      key :summary, 'Get rounds list event tournament'
+      key :description, 'Event Catalog'
+      key :operationId, 'tournamentsRondsList'
+      key :produces, ['application/json',]
+      key :tags, ['events']
+      parameter do
+        key :name, :event_id
+        key :in, :path
+        key :required, true
+        key :type, :integer
+        key :format, :int64
+      end
+      parameter do
+        key :name, :category_id
+        key :in, :query
+        key :required, true
+        key :type, :integer
+        key :format, :int64
+      end
+      parameter do
+        key :name, :bracket_id
+        key :in, :query
+        key :required, true
+        key :type, :integer
+        key :format, :int64
+      end
+      response 200 do
+        key :description, 'Raound Respone'
+        schema do
+          key :type, :object
+          property :data do
+            key :type, :array
+            items do
+              key :'$ref', :Raund
+            end
+            key :description, "Information container"
+          end
+        end
+      end
+      response 401 do
+        key :description, 'not authorized'
+        schema do
+          key :'$ref', :ErrorModel
+        end
+      end
+      response :default do
+        key :description, 'unexpected error'
+      end
+    end
+  end
   def rounds_list
     tournament = Tournament.where(:event_id => @event.id).where(:event_bracket_id => players_list_params[:bracket_id])
                      .where(:category_id => players_list_params[:category_id]).first_or_create!
