40d59d4f12187f602996b31cf512698398e3b532
daniel flores sosa <danieldflrss@gmail.com>
2018-10-01 15:49:19 -0600

    How score my matches

--- app/serializers/round_my_matches_serializer.rb
+++ app/serializers/round_my_matches_serializer.rb
@@ -1,5 +1,6 @@
 class RoundMyMatchesSerializer < ActiveModel::Serializer
-  attributes :id, :index, :status, :matches
+  attributes :id, :index, :status
+  has_many :matches, :serializer => MatchSerializer
   def matches
     object.matches.where("team_a_id = ? OR team_b_id = ?",  object.for_team_id,  object.for_team_id)
   end
