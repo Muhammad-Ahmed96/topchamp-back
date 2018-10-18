54a9c849ce708bd28c68436d0556ab40f7e34ecc
daniel flores sosa <danieldflrss@gmail.com>
2018-10-18 09:58:48 -0600

    validate match id

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,7 +1,9 @@
 class MatchSerializer < ActiveModel::Serializer
   def initialize(*args)
     super
-    instance_options[:match_id] = object.id
+    unless object.nil?
+      instance_options[:match_id] = object.id
+    end
   end
   attributes :id, :index, :round_id, :team_a_id, :team_b_id,:status, :seed_team_a, :seed_team_b, :match_number,
              :court, :date, :start_time, :end_time, :team_winner_id
