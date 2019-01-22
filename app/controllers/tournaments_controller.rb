0d6b4b05305eeb6e7404f69a8276ec9629df9984
daniel flores sosa <danieldflrss@gmail.com>
2019-01-22 09:21:22 -0700

    change event id contest

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -549,7 +549,7 @@ class TournamentsController < ApplicationController
   end
   def details
     tournament = Tournament.where(:event_id => @event.id).where(:event_bracket_id => players_list_params[:bracket_id])
-                     .where(:category_id => players_list_params[:category_id]).first_or_create!
+                     .where(:event_contest_category_id => players_list_params[:category_id]).first_or_create!
     json_response_serializer(tournament, TournamentWithTeamsSerializer)
   end
   private
