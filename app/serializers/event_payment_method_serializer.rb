65b9e6ae111b7fe3f11358957422a08dc7411a8e
daniel flores sosa <danieldflrss@gmail.com>
2019-01-23 16:11:52 -0700

    save service fee

--- app/serializers/event_payment_method_serializer.rb
+++ app/serializers/event_payment_method_serializer.rb
@@ -1,4 +1,4 @@
 class EventPaymentMethodSerializer < ActiveModel::Serializer
   attributes :id, :event_id, :enrollment_fee, :bracket_fee, :currency
-  belongs_to :processing_fee, serializer: ProcessingFeeSerializer
+  #belongs_to :processing_fee, serializer: ProcessingFeeSerializer
 end
