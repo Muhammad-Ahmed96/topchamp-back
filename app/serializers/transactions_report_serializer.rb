2b4098f89a4334e4f2e9dc9b4ef1803323dea890
daniel flores sosa <danieldflrss@gmail.com>
2019-03-25 14:14:43 -0700

    serializer - TW-1193

--- app/serializers/transactions_report_serializer.rb
+++ app/serializers/transactions_report_serializer.rb
@@ -1,4 +1,4 @@
 class TransactionsReportSerializer < ActiveModel::Serializer
   attributes :user_id, :payment_transaction_id , :player_name, :authorize_fee, :top_champ_account, :top_champ_fee, :director_receipt,
-             :amount, :event_id
+             :amount, :event_id, :available_refund, :refund_total
 end
