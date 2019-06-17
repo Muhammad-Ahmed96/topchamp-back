6aba517d313574ad93d2d99b6a23e10c3e10f4d3
daniel flores sosa <danieldflrss@gmail.com>
2019-06-17 12:26:17 -0600

    gender filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -175,7 +175,7 @@ class UsersController < ApplicationController
     end
 
     users = User.my_order(column, direction).search(search).in_role(role).birth_date_in(birth_date)
-                .in_status(status).first_name_like(first_name).last_name_like(last_name).gender_like(gender)
+                .in_status(status).first_name_like(first_name).last_name_like(last_name).gender_in(gender)
                 .email_like(email).last_sign_in_at_like(last_sign_in_at).state_like(state).city_like(city)
                 .sport_in(sport_id).contact_information_order(column_contact_information, direction).sports_order(column_sports, direction)
     if paginate.to_s == "0"
