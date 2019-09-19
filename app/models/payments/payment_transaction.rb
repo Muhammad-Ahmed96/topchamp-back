ddebee29fe1fb190e2fea744ef6e62f50882881f
Daniel Flores Sosa <danieldflrss@gmail.com>
2019-09-19 14:47:10 -0600

    show tax after discount

--- app/models/payments/payment_transaction.rb
+++ app/models/payments/payment_transaction.rb
@@ -4,5 +4,13 @@ module Payments
     attr_accessor :event_payments
     belongs_to :transactionable, polymorphic: true, :optional => true
     has_many :details, :class_name => 'Payments::PaymentTransactionDetail'
+
+    def total_tax
+      tax = self.tax
+      if self.amount == 0 and  self.discount > 0
+        tax = self.discount - self.total
+      end
+      tax.round(2)
+    end
   end
 end
