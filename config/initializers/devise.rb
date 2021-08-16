971c7d120f16e7b6ce07d53ccb5853a0705d08e5
Muzzamil123 <muzamil.mujahid11@gmail.com>
2021-08-16 16:13:31 +0500

    mails added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'testomator@amplemind.com'
+  config.mailer_sender = 'marc@shadowfoxtrot.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'MyDeviseMailer'
